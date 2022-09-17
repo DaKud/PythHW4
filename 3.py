a6f1b9ecf2df7f0744d78ee9d4b6f77da384d6a6
DaKu <do.holyada@gmail.com>
2022-09-17 23:16:49 +0300

    3rd task

--- 3.py
+++ 3.py
@@ -1,2 +1,9 @@
 # Задайте последовательность чисел. Напишите программу, 
 # которая выведет список неповторяющихся элементов исходной последовательности.
+
+raw = input('Insert numbers with a space:  ').split()
+unique = []
+for i in raw: 
+        if raw.count(i) == 1: unique.append(i)
+# print(f'Unique element: {unique}')
+print ('Unique element(s): ',','.join(unique))
